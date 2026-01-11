27b81122966d44618ef43a843681f0e7b60b4bcd
Baran Arda Banarlı <baranardabanarli@gmail.com>
2026-01-11 18:15:27 +0300

    Kimlik bilgileri duzenlendi

--- Veri Yapilari ve Algoritmalar Final Bonus.c
+++ Veri Yapilari ve Algoritmalar Final Bonus.c
@@ -18,25 +18,22 @@ void takas(int *ap, int *bp) {
 	*bp = gecici;
 }
 
-// baloncuk siralama optimizasyonu
-// takas olup olmadigi kontrol edilerek
-// daha az iterasyon gerceklesmesi saglanabilir
 void baloncuk_sirala(int dizi[], int BOYUT) {
 	int i, j;
-	int takas_oldu;  // bayrak (flag)
+	int takas_oldu;
 	
-	for (i=1; i<BOYUT; i++) { // iterasyon islemleri
-		takas_oldu = 0;    // bayrak sifirlama (reset)
+	for (i=1; i<BOYUT; i++) { 
+		takas_oldu = 0;    
 		for (j=0; j<BOYUT-1; j++) {
 			if (dizi[j] > dizi[j+1]) {
 				takas(dizi+j , dizi+j+1);
-				takas_oldu = 1;  // takas gerceklesti
+				takas_oldu = 1;  
 			}
 		}
 		printf("iter.%2d: ", i);
 		dizi_yazdir(dizi, BOYUT);
 		if (takas_oldu == 0) {
-			break;    // herhangi bir takas yoksa siralama bitmistir
+			break;    
 		}
 	}
 }
@@ -44,19 +41,15 @@ void baloncuk_sirala(int dizi[], int BOYUT) {
 int ikili_arama(int dizi[], int BOYUT, int search, int ilk_ind, int son_ind) {
 	int orta_ind = (ilk_ind + son_ind) / 2;
 	
-	// aradigimiz eleman dizinin orta indisli elemanina esit ise
 	if (search == dizi[orta_ind]) {
 		return orta_ind;
 	}
-	// aradigimiz eleman dizinin orta indisli elemanindan buyuk ise
 	else if (search > dizi[orta_ind] && (son_ind-ilk_ind) > 0 ) {
 		return ikili_arama(dizi, BOYUT, search, orta_ind+1, son_ind);
 	}
-	// aradigimiz eleman dizinin orta indisli elemanindan kucuk ise
 	else if (search < dizi[orta_ind] && (son_ind-ilk_ind) > 0) {
 		return ikili_arama(dizi, BOYUT, search, 0, orta_ind-1);
 	}
-	// istenen kosullar saglanamadi --> aranan deger bulunamadi
 	else {
 		return -1;
 	}
@@ -67,17 +60,15 @@ int main() {
 	int N = sizeof(A) / sizeof(A[0]);
 	int aranan, sonuc;
 	
-	dizi_yazdir(A, N);      // orijinal dizi
+	dizi_yazdir(A, N);   
 	
-	// once siralamamiz gerekiyor yoksa ikili arama calismaz
 	baloncuk_sirala(A, N);  
 	
-	dizi_yazdir(A, N);      // siralanmis sonuc dizisi
+	dizi_yazdir(A, N);    
 	
 	printf("Aramak istediginiz deger: ");
 	scanf("%d", &aranan);
 	
-	// ikili_arama(dizi adi, eleman sayisi, aranan eleman, ilk indis, son indis)
 	sonuc = ikili_arama(A, N, aranan, 0, N-1);
 	
 	if (sonuc == -1) {
